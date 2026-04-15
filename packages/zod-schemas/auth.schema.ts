57fd7c71e112255b9a38003e7aa21a4d8bdb201d
lakshyakumar90 <lakshyakumar5023@gmail.com>
2026-04-15 11:25:23 +0530

    feat(auth): enhance authentication schemas with password validation and confirmation

--- packages/zod-schemas/auth.schema.ts
+++ packages/zod-schemas/auth.schema.ts
@@ -1,13 +1,51 @@
 import { z } from "zod";
 
-export const RegisterSchema = z
-  .object({
-    username: z
-      .string()
-      .min(3, { message: "Username must be at least 3 characters long" }),
-    email: z.email({ message: "Invalid email address" }),
-    password: z
-      .string()
-      .min(8, { message: "Password must be at least 8 characters long" }),
+const passwordSchema = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .max(64, { message: "Password must be at most 64 characters long" })
+  .regex(/[A-Z]/, {
+    message: "Password must contain at least one uppercase letter",
   })
-  .strict();
+  .regex(/[a-z]/, {
+    message: "Password must contain at least one lowercase letter",
+  })
+  .regex(/[0-9]/, { message: "Password must contain at least one number" })
+  .regex(/[!@#$%^&*()]/, {
+    message: "Password must contain at least one special character",
+  });
+
+const RegisterSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: "Name must be at least 3 characters long" })
+      .max(64, { message: "Name must be at most 64 characters long" }),
+    email: z.email({ message: "Invalid email address" }),
+    password: passwordSchema,
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    },
+  );
+
+const LoginSchema = z.object({
+  email: z.email({ message: "Invalid email address" }),
+  password: passwordSchema,
+});
+
+const RefreshTokenSchema = z.object({
+  refreshToken: z.string({ message: "Invalid refresh token" }),
+});
+
+export { RegisterSchema, LoginSchema, RefreshTokenSchema };
+
+export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
+export type LoginSchemaType = z.infer<typeof LoginSchema>;
+export type RefreshTokenSchemaType = z.infer<typeof RefreshTokenSchema>;
